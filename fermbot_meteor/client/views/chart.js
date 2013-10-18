ca88331c5be78d42418c321a42b1b55460f8cdaa
Quinn Scripter <qscripter@gmail.com>
2013-10-18 12:06:14 -0400

    changes to make chart responsive

--- fermbot_meteor/client/views/chart.js
+++ fermbot_meteor/client/views/chart.js
@@ -1,6 +1,68 @@
+function respChart(selector, data, options){
+
+        // Define default option for line chart
+        var option = {
+                scaleOverlay : false,
+                scaleOverride : false,
+                scaleSteps : null,
+                scaleStepWidth : null,
+                scaleStartValue : null,
+                scaleLineColor : "rgba(0,0,0,.1)",
+                scaleLineWidth : 1,
+                scaleShowLabels : true,
+                scaleLabel : "<%=value%>",
+                scaleFontFamily : "'proxima-nova'",
+                scaleFontSize : 10,
+                scaleFontStyle : "normal",
+                scaleFontColor : "#909090",
+                scaleShowGridLines : true,
+                scaleGridLineColor : "rgba(0,0,0,.05)",
+                scaleGridLineWidth : 1,
+                bezierCurve : true,
+                pointDot : true,
+                pointDotRadius : 3,
+                pointDotStrokeWidth : 1,
+                datasetStroke : true,
+                datasetStrokeWidth : 2,
+                datasetFill : true,
+                animation : true,
+                animationSteps : 60,
+                animationEasing : "easeOutQuart",
+                onAnimationComplete : null
+        };
+
+        // check if the option is override to exact options 
+        // (bar, pie and other)
+        if (options === false || options === null) {
+                options = option;
+        }
+
+        // get selector by context
+        var ctx = selector.get(0).getContext("2d");
+        // pointing parent container to make chart js inherit its width
+        var container = $(selector).parent();
+
+        // enable resizing matter
+        $(window).resize( generateChart );
+
+
+
+        // this function produce the responsive Chart JS
+        function generateChart(){
+                // make chart width fit with its container
+                var ww = selector.attr('width', $(container).width() );
+                // Initiate new chart or Redraw
+                return new Chart(ctx).Line(data, options);
+        }
+
+        // run function - render chart at first load
+        return generateChart();
+
+}
+
 function drawChart (readings) {
-	var ctx = document.getElementById("myChart").getContext("2d");
-	if (ctx && readings.length > 0) {
+	var $chart = $('#myChart');
+	if ($chart && readings.length > 0) {
 		var labels = [];
 		var temps = [];
 		for (var i=0; i < readings.length; i++) {
@@ -23,7 +85,7 @@ function drawChart (readings) {
 				}
 			]
 		};
-		var chart = new Chart(ctx).Line(data);
+		var chart = respChart($chart, data);
 	}
 }
 
--- sampleData.js
+++ sampleData.js
@@ -1,16 +1,21 @@
-data = [
-{"date_time":"2013-10-17T16:00:03.865197","temp_f":71.2616,"sensor":"28-0000043aa9b1","_id":"FJxxkyHoz9v9FYesZ"},
-{"date_time":"2013-10-17T16:30:04.065083","temp_f":71.2616,"sensor":"28-0000043aa9b1","_id":"RuvWj3hMacYTnzkZA"},
-{"date_time":"2013-10-17T17:00:03.915060","temp_f":71.375,"sensor":"28-0000043aa9b1","_id":"F87k8BoRkLcsa9Pvf"},
-{"date_time":"2013-10-17T17:30:03.865090","temp_f":71.375,"sensor":"28-0000043aa9b1","_id":"HtFhDzWfTHaD3P2Wf"},
-{"date_time":"2013-10-17T18:00:03.875342","temp_f":71.375,"sensor":"28-0000043aa9b1","_id":"bB3t882CsedsdyCyW"},
-{"date_time":"2013-10-17T18:30:03.845176","temp_f":71.48660000000001,"sensor":"28-0000043aa9b1","_id":"WjaaKGffM2m9jCviB"},
-{"date_time":"2013-10-17T19:00:03.695080","temp_f":71.48660000000001,"sensor":"28-0000043aa9b1","_id":"dauvX62zJW8xchRWi"},
-{"date_time":"2013-10-17T19:30:03.775082","temp_f":71.48660000000001,"sensor":"28-0000043aa9b1","_id":"ZjPQ26C9b4C4eWBPT"},
-{"date_time":"2013-10-17T20:00:03.735106","temp_f":71.6,"sensor":"28-0000043aa9b1","_id":"qRKmPjoWrB2ZRuqZq"},
-{"date_time":"2013-10-17T20:30:03.355070","temp_f":71.6,"sensor":"28-0000043aa9b1","_id":"etPXQpDgTaR5hZwRN"},
-{"date_time":"2013-10-17T21:00:04.768414","temp_f":71.825,"sensor":"28-0000043aa9b1","_id":"TzmcsMSFLfCctXk2Z"},
-{"date_time":"2013-10-17T21:30:04.088378","temp_f":71.7116,"sensor":"28-0000043aa9b1","_id":"bthy3Au3svroP9965"},
-{"date_time":"2013-10-17T21:37:29.308481","temp_f":71.7116,"sensor":"28-0000043aa9b1","_id":"b3E7WdZrA6kyFw39G"},
-{"date_time":"2013-10-17T22:00:03.388384","temp_f":71.825,"sensor":"28-0000043aa9b1","_id":"btSpv2sykf2M3TjYy"}
-];
+function insertSampleData () {
+	data = [
+		{"date_time":"2013-10-17T16:00:03.865197","temp_f":71.2616,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T16:30:04.065083","temp_f":71.2616,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T17:00:03.915060","temp_f":71.375,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T17:30:03.865090","temp_f":71.375,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T18:00:03.875342","temp_f":71.375,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T18:30:03.845176","temp_f":71.48660000000001,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T19:00:03.695080","temp_f":71.48660000000001,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T19:30:03.775082","temp_f":71.48660000000001,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T20:00:03.735106","temp_f":71.6,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T20:30:03.355070","temp_f":71.6,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T21:00:04.768414","temp_f":71.825,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T21:30:04.088378","temp_f":71.7116,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T21:37:29.308481","temp_f":71.7116,"sensor":"28-0000043aa9b1"},
+		{"date_time":"2013-10-17T22:00:03.388384","temp_f":71.825,"sensor":"28-0000043aa9b1"}
+	];
+	for (var i=0; i<data.length; i++) {
+		Readings.insert(data[i]);
+	}
+}
